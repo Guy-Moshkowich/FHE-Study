2d000976bb5322deade6bd2a7bbe1eaee6cb56f6
GUY MOSHKOWICH <guy.moshkowich@ibm.com>
2022-09-09 08:47:23 +0300

    key switch debugging: add many asserts in the key switching algorithm

--- CKKS/ckks_test.py
+++ CKKS/ckks_test.py
@@ -1,8 +1,7 @@
 import unittest
 from ckks import CKKS
-from numpy.polynomial import Polynomial
 from RLWE.ring_element import RingElement
-import random
+
 
 class TestCkks(unittest.TestCase):
 
@@ -16,86 +15,72 @@ class TestCkks(unittest.TestCase):
         ct = self.ckks.encrypt(plaintext)
         self.assert_equal(ct, self.ckks.secret_key, plaintext, 20)
 
-        # plaintext_decrypted = self.ckks.decrypt(ct, self.ckks.secret_key)
-        # diff = plaintext_decrypted - plaintext
-        # self.assertTrue(diff.norm() <= self.max_error)
+    def test_encrypt_decrypt_bad_secret_key(self):
+        self.ckks = CKKS(logn=10, q=1000)
+        plaintext_expected = RingElement.random(self.ckks.n, self.ckks.q)
+        ct = self.ckks.encrypt(plaintext_expected)
+        bad_secret_key = RingElement.random(self.ckks.n, self.ckks.q)
+        plaintext_actual = self.ckks.decrypt(ct, bad_secret_key)
+        diff = plaintext_actual - plaintext_expected
+        self.assertTrue(diff.canonical_norm() > 1000, diff.canonical_norm())
+
 
     def test_encrypt_core_binary_a(self):
         self.ckks = CKKS(logn=10, q=1000)
         plaintext = RingElement.random(self.ckks.n, self.ckks.q)
-        a = RingElement.random_binary(self.ckks.n, self.ckks.q, max_range=3)
+        a = RingElement.random_binary(self.ckks.n, self.ckks.q)
         e = RingElement.small_gauss(self.ckks.n, self.ckks.q)
         sk1 = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
         ct = self.ckks.encrypt_core(plaintext, a, sk1, e)
-        self.assert_equal(ct, sk1, plaintext, 20)
+        self.assert_equal(ct, sk1, plaintext, 25)
 
-    def test_generate_swk_core_with_small_a(self):
+    def test_generate_swk_core_with_binary_a(self):
         self.ckks = CKKS(logn=10, q=1000)
-        a = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
+        a = RingElement.random_binary(self.ckks.n, self.ckks.q)
         e = RingElement.small_gauss(self.ckks.n, self.ckks.q)
         s = RingElement.random(self.ckks.n, self.ckks.q)
         s_prime = RingElement.random(self.ckks.n, self.ckks.q)
         swk = self.ckks.generate_swk_core(s_prime, s, a, e)
-        self.assert_equal(swk, s_prime, s, 10)
+        self.assert_equal(swk, s, s_prime, 20)
 
     def test_generate_swk(self):
         s = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
         s_prime = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
         swk = self.ckks.generate_swk(s_prime, s)
-        self.assert_equal(swk, s_prime, s, 20)
+        self.assert_equal(swk, s_prime, s, 50)
 
 
-        # plaintext_decrypted = self.ckks.decrypt(swk, s)
-        # diff = plaintext_decrypted - s_prime
-        # result = diff.norm() <= self.max_error
-        # self.assertTrue(result, diff.norm())
-
-    def test_switch_key_for_small_a(self):
-        self.ckks = CKKS(logn=10, q=10000)
+    def test_switch_key_for_binary_a(self):
+        self.ckks = CKKS(logn=10, q=1000)
         plaintext = RingElement.random(self.ckks.n, self.ckks.q)
         s = RingElement.random(self.ckks.n, self.ckks.q)
         s_prime = RingElement.random(self.ckks.n, self.ckks.q)
-        a_prime = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
+        a_prime = RingElement.random_binary(self.ckks.n, self.ckks.q)
         e_prime = RingElement.small_gauss(self.ckks.n, self.ckks.q)
         swk_from_s_prime_to_s = self.ckks.generate_swk_core(s_prime, s, a_prime, e_prime)
-        # swk_from_s_prime_to_s = self.ckks.generate_swk(s_prime, s)
-        self.assert_equal(swk_from_s_prime_to_s, s_prime, s, 20)
+        self.assert_equal(swk_from_s_prime_to_s, s, s_prime, 20)
 
-        a = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
-        e = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
+        a = RingElement.random_binary(self.ckks.n, self.ckks.q)
+        e = RingElement.small_gauss(self.ckks.n, self.ckks.q)
 
-        print("e_prime=", e_prime)
-        print("a=", a)
-        print("e_prime*a=",e_prime*a)
-        # assert (e_prime*a).norm() < self.max_error, "max|e'*a|="+str((e_prime*a).norm()) + ", avg|e'*a|="+str((e_prime*a).norm_avg())
+        print("|s_prime|=", s_prime.canonical_norm())
+        print("|e_prime|=", e_prime.canonical_norm())
+        print("|e|=", e.canonical_norm())
+        print("|a|=", a.canonical_norm())
+        print("|e_prime*a|=",(e_prime*a).canonical_norm())
+        print("|a_prime*a|=",(a_prime*a).canonical_norm())
 
         ct_wrt_s_prime = self.ckks.encrypt_core(plaintext, a, s_prime, e)
-        self.assert_equal(ct_wrt_s_prime, s_prime, plaintext, 10)
+        self.assert_equal(ct_wrt_s_prime, s_prime, plaintext, 30)
 
-        ct_wrt_s = self.ckks.switch_key(ct_wrt_s_prime, swk_from_s_prime_to_s)
-        # self.assert_equal(ct_wrt_s, s, plaintext, 10)
+        ct_wrt_s = self.ckks.switch_key(ct_wrt_s_prime, swk_from_s_prime_to_s,s_prime,s, plaintext)
+        print("|ct_wrt_s_a|=",ct_wrt_s[1].canonical_norm())
+        print("ct_wrt_s[1].poly.coef[0]: ",  ct_wrt_s[1].poly.coef[0])
+        print("(a_prime*a).poly.coef[0]: ", (a_prime*a).poly.coef[0])
+        assert ct_wrt_s[1].poly.coef[0] ==-(a_prime*a).poly.coef[0] % self.ckks.q
 
+        self.assert_equal(ct_wrt_s, s, plaintext, 100)
 
-    # def test_switch_key(self):
-    #     for i in range(100):
-    #         print('i: ', i)
-    #         self.ckks = CKKS(logn=10, q=2**10, max_added_noise=6)
-    #         plaintext = RingElement.random(self.ckks.n, self.ckks.q, max_range=self.ckks.q//10)
-    #         s = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
-    #         s_prime = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
-    #         swk = self.ckks.generate_swk(s_prime, s)
-    #         print('swk: ', swk[1].poly.coef[:10])
-    #
-    #         a = RingElement.random(self.ckks.n, self.ckks.q, max_range=1)
-    #         # print('a: ',a)
-    #         e = RingElement.random(self.ckks.n, self.ckks.q, max_range=5)
-    #         ct = self.ckks.encrypt_core(plaintext, a, s_prime, e)
-    #         print('ct: ', ct[1].poly.coef[:10])
-    #         ct_wrt_s = self.ckks.switch_key(ct, swk)
-    #         print('ct_wrt_s: ', ct_wrt_s[1].poly.coef[:10])
-    #
-    #         plaintext_decrypted = self.ckks.decrypt(ct_wrt_s, s)
-    #         self.assert_poly_diff(plaintext_decrypted, plaintext, 10)
 
     def assert_equal(self, ctx, sk, plaintext_expected, max_error):
         plaintext_actual = self.ckks.decrypt(ctx, sk)
--- RLWE/ring_element.py
+++ RLWE/ring_element.py
@@ -7,12 +7,16 @@ import Utils.utils
 
 
 class RingElement:
+    primitive_roots =[]
 
     def __init__(self, poly: Polynomial, m, mod):
         self.mod = mod
         self.m = m
         self.phi_m = self.get_cyclotomic()
         self.poly = self.modulo(poly, mod)
+        if len(RingElement.primitive_roots) == 0:
+            RingElement.primitive_roots = Utils.utils.get_nth_primitive_roots_of_unity(self.m)
+
 
     def get_cyclotomic(self): #X^{m/2}+1
         phi_m = [0]*(self.m//2 + 1)
@@ -54,6 +58,11 @@ class RingElement:
         return cls(poly, m, mod)
 
     @classmethod
+    def random_binary(cls, m, mod):
+        return RingElement.random(m, mod, max_range=1)
+
+
+    @classmethod
     def small_gauss(cls, m, mod, mu=1, sigma=0.5):
         small_poly = Polynomial([int(random.gauss(mu=1, sigma=0.5)-1) for i in range(0, m//2)])
         return cls(small_poly, m, mod)
@@ -71,24 +80,13 @@ class RingElement:
         compose_poly1d = np.poly1d(self.poly.coef[::-1])(g_poly1d)
         return RingElement(Polynomial(compose_poly1d.coefficients[::-1]), self.m, self.mod)
 
-
     def canonical_norm(self):
-        primitive_roots = Utils.utils.get_nth_primitive_roots_of_unity(self.m)
         evals_abs = []
-        for x in primitive_roots:
+        for x in RingElement.primitive_roots:
             coef_transformed = [self.transform_mod_div_2_center(x) for x in self.poly.coef]
             eval = numpy.polynomial.polynomial.polyval(x, coef_transformed)
             evals_abs.append(abs(eval))
         return max(evals_abs)
-
-    # def get_normed_coef(self):
-    #     normalize_coef = []
-    #     for x in self.poly.coef:
-    #         if x <= self.mod // 2:
-    #             normalize_coef.append(x)
-    #         else:
-    #             normalize_coef.append(abs(self.mod - x))
-    #     return normalize_coef
 
     def transform_mod_div_2_center(self, element):
         if element <= self.mod // 2:
